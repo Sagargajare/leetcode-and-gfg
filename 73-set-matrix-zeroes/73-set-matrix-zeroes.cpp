ddd2d38f3bde531e2ecb9ea9814b339cf6f6d0d0
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-05-30 08:48:24 +0530

    Time: 19 ms (57.66%), Space: 13.3 MB (54.60%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -12,13 +12,17 @@ public:
             }
         }
         vector<int> zeros(matrix[0].size(),0);
-        for(auto x:row){
-            matrix[x] = zeros;
-        }
+        // for(auto x:row){
+        //     matrix[x] = zeros;
+        // }
         for(int i = 0;i<matrix.size();i++){
             for(int j = 0;j<matrix[0].size();j++){
                 if(col.find(j) != col.end()){
                     matrix[i][j] = 0;
+                    
+                }
+                if(row.find(i) != row.end()){
+                    matrix[i] = zeros;
                     
                 }
                 
