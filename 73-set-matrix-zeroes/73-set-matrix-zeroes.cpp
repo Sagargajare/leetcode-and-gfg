724ad56653c961f14c53adc56c7a66c15123861a
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-05-30 08:50:22 +0530

    Time: 26 ms (22.82%), Space: 13.3 MB (19.35%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -11,20 +11,10 @@ public:
                 }
             }
         }
-        vector<int> zeros(matrix[0].size(),0);
-        // for(auto x:row){
-        //     matrix[x] = zeros;
-        // }
+        // vector<int> zeros(matrix[0].size(),0);
         for(int i = 0;i<matrix.size();i++){
             for(int j = 0;j<matrix[0].size();j++){
-                if(col.find(j) != col.end()){
-                    matrix[i][j] = 0;
-                    
-                }
-                if(row.find(i) != row.end()){
-                    matrix[i] = zeros;
-                    
-                }
+                if(row.count(i) || col.count(j))matrix[i][j]=0; 
                 
             }
         }
