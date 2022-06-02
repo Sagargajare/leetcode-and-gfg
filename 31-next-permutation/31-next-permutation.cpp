a9007a3ae16137c2ecc35b9b847f89ed767a6e68
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-06-02 22:03:51 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (75.38%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(), nums.end());
+    
+    void nextPermutation(vector<int>& a) {
+       int n = a.size();
+        if(n==1) return;
+        int i;
+        for(i = n-2; i>=0;i--){
+            if(a[i]>=a[i+1]) continue;
+            else break;
+        }
+        if(i<0){
+            reverse(a.begin(),a.end());
+            return;
+        }
+        int j = i+1;
+        while(j<n && a[j]>a[i]){
+            j++;
+        }
+        j--;
+        swap(a[i],a[j]);
+        reverse(a.begin()+(i+1),a.end());
+        return;
        
     }
 };
