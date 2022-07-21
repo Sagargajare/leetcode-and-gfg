44b39ebc1fecdd7b564a4700384cf04302edd54f
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-07-21 20:45:49 +0530

    Time: 14 ms (81.75%), Space: 11.1 MB (22.12%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mp;
+         map<int,int> mp;
         vector<int> output;
         for(int i = 0;i<nums.size();i++){
             if(mp.find(target - nums[i]) != mp.end()){
-                cout<<nums[i]<<" "<<(target - nums[i])<<" ";
                 output.push_back(i);
                 output.push_back(mp.find(target - nums[i])->second);
                 return output;
